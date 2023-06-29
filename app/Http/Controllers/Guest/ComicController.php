7022959de2a561b5c7787c82faba88bbcadfdabf
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-29 16:53:05 +0200

    add card in index.blade.php

--- app/Http/Controllers/Guest/ComicController.php
+++ app/Http/Controllers/Guest/ComicController.php
@@ -15,7 +15,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        $comics = Comic::paginate(2);
+        $comics = Comic::paginate(3);
         return view('comics.index', compact('comics'));
     }
 
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.base')
 
 @section('contents')
-    <h1>I nostri fumetti!</h1>
+    <div class="container">
+        <h1 class="text-center">I nostri fumetti!</h1>
+
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col-4">
+                <div class="card">
+                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        <p class="card-text">{{ $comic->type }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        
+    </div>
 @endsection
