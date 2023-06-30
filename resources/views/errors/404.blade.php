7b4dbb45436a4d17bb972638c36b8fbd833e6538
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-30 17:49:20 +0200

    fix error 404 with style and text

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.base')
+
+@section('contents')
+    <div class="container">
+        <h1>OOOOOOOOPS!!!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit exercitationem dolor delectus magnam odio ipsa sed nisi rem accusantium voluptas, vitae corporis, velit expedita! Soluta assumenda voluptatem praesentium non tempore.
+        Nobis autem nemo nisi tempora modi eum? Dicta dolores ducimus veritatis quaerat at explicabo obcaecati amet sed inventore, repellendus quibusdam earum id sint doloribus. Vero voluptates temporibus explicabo id iste.
+        Magnam facilis atque aut praesentium. Ducimus dolorum totam ipsa iusto ut cumque similique suscipit architecto eius inventore rem fuga numquam quos consequatur hic, deserunt aliquam nulla dicta excepturi nam placeat!
+        Dignissimos, cumque? Consectetur ratione quasi exercitationem quos blanditiis quas quidem voluptatem veritatis sapiente debitis sequi architecto voluptatibus, dignissimos, nemo a, sint quis ab repellat dolorum suscipit culpa alias praesentium possimus?
+        Possimus nisi, natus, corporis repudiandae quidem quo numquam aut, harum sed explicabo expedita voluptate nulla quas cumque dignissimos! Tenetur fugit beatae quaerat, voluptatem culpa itaque necessitatibus dolorem pariatur. Deleniti, consequatur.
+        Suscipit eveniet sapiente dolorem culpa incidunt atque quaerat omnis unde fugit. Eius dignissimos minus perspiciatis eaque odio saepe. Totam repellendus non sed obcaecati blanditiis eos fuga aut sint, pariatur consequatur!
+        In, nesciunt reprehenderit soluta deleniti velit vitae mollitia amet molestiae excepturi eius? Delectus ratione necessitatibus consequatur suscipit autem illo neque corrupti nam tempore! Tempore odit doloremque deleniti quisquam quibusdam quaerat?
+        Veniam atque iusto, magnam illo adipisci rem facilis iure dicta eius repellat blanditiis assumenda ad consequuntur molestiae sunt eveniet? Cumque totam quas voluptas harum quisquam, sapiente iusto beatae id voluptatum?
+        Commodi vitae asperiores perspiciatis quasi, mollitia adipisci rerum veniam quam quis accusantium expedita unde facere tempore veritatis ab assumenda blanditiis facilis labore sequi molestias consequatur culpa totam! Quo, nesciunt necessitatibus!
+        Alias illo soluta sunt obcaecati voluptatibus dicta esse ratione consequuntur, recusandae illum corrupti omnis inventore, ab reiciendis quam qui vero! Accusantium ut iste molestiae asperiores libero autem odio ducimus aspernatur?</p>
+        <br>
+        <br>
+        <h5>Vuoi tornare alla Home page?</h5>
+        <a href="{{ route('home') }}" class="btn btn-primary">Clicca Qui!!</a> 
+    </div>   
+@endsection
+
